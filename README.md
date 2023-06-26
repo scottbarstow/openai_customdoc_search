f925c7e3ee4c0a12b1d12ea8f44194ec2bf3517e
sbarstow <sbarstow@gmail.com>
2023-06-26 13:16:15 -0400

    initial checkin

---
+++ README.md
@@ -0,0 +1,32 @@
+# Open AI and Custom PDF document search demo
+
+This project uses OpenAI, Langchain and Streamlit to demonstrate a 
+working website where you can upload PDFs, have the vectors populated, and
+then do a semantic search of the contents.
+
+It uses the FAISS in-memory data store for vector storage
+
+## To run the demo
+
+```
+pip install openai
+pip install langchain
+pip install PyPDF2
+pip install faiss-cpu
+pip install streamlit
+```
+
+Next, you'll need to have your OpenAI API Key available. This can be either an environment variable `OPENAI_API_KEY` or 
+it will look for the key in a file in your home directory called `openai-api-key.txt`
+
+### Running the Streamlit app
+
+```
+streamlit run site.py
+```
+
+### Running the console app
+Note that you'll need to update the document name and the query with your specific info
+```
+python main.py
+```
---
+++ main.py
@@ -0,0 +1,40 @@
+## This is a simple prototype of using Langchain on a PDF document to search it. 
+## The site.py is a more fulsome, realtime example of using Langchain to search a PDF document.
+
+import os
+
+from PyPDF2 import PdfReader
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.vectorstores import FAISS
+from openai_utils import get_openai_api_key
+from langchain.chains.question_answering import load_qa_chain
+from langchain.llms import OpenAI
+
+
+reader = PdfReader("./yourpdf.pdf")
+
+raw_text = ''
+for i, page in enumerate(reader.pages):
+    text = page.extract_text()
+    if text:
+        raw_text += text
+
+text_splitter = CharacterTextSplitter(        
+    separator = "\n",
+    chunk_size = 1000,
+    chunk_overlap  = 200,
+    length_function = len,
+)
+texts = text_splitter.split_text(raw_text)
+
+openai_api_key = get_openai_api_key()
+embeddings = OpenAIEmbeddings(openai_api_key=openai_api_key)
+
+docsearch = FAISS.from_texts(texts, embeddings)
+
+chain = load_qa_chain(OpenAI(openai_api_key=openai_api_key), chain_type="stuff")
+
+query = "what are the different kinds of teams a product team can use?"
+docs = docsearch.similarity_search(query)
+print(chain.run(input_documents=docs, question=query))
